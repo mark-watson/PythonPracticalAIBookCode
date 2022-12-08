bbbbd534f0bceb0653fb0888e27048b94ce421f4
Mark Watson <markw@markwatson.com>
2022-12-08 15:41:55 -0700

    starting to add Swi-Prolog examples

---
+++ swi-prolog/n_queens.py
@@ -0,0 +1,11 @@
+from swiplserver import PrologMQI
+from pprint import pprint
+
+with PrologMQI() as mqi:
+    with mqi.create_thread() as prolog_thread:
+        prolog_thread.query("use_module(library(clpfd)).")
+        prolog_thread.query("[n_queens].")
+        result = prolog_thread.query("n_queens(8, Qs), label(Qs).")
+        pprint(result)
+        print(len(result))
+
