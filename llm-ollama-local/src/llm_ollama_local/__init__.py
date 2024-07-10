ac764ff40ed04100b6f1f40f95be3502ff1fdd66
Mark Watson <markw@markwatson.com>
2024-07-10 15:11:35 -0700

    commented out debug printout

--- llm-ollama-local/src/llm_ollama_local/__init__.py
+++ llm-ollama-local/src/llm_ollama_local/__init__.py
@@ -8,7 +8,7 @@ model_name = "llama3:latest"
 
 def completion(prompt: str) -> str:
     response = client.generate(model_name, prompt)
-    print(response)
+    #print(response)
     return response['response']
 
 def completion_with_context(prompt: str, context_file_path: str) -> str:
@@ -17,6 +17,6 @@ def completion_with_context(prompt: str, context_file_path: str) -> str:
     
     full_prompt = f"Context:\n{context}\n\nPrompt:\n{prompt}"
     response = client.generate(model_name, full_prompt)
-    print(response)
+    #print(response)
     return response['response']
 
